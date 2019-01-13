4326651a31c0d3cac95a250e9f2b186df6e8fe3b
Ezana Bahta <ezanab2@gmail.com>
2019-01-13 11:47:57 -0500

    links for footer filled in with appropriate links.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,10 +8,10 @@ class Footer extends Component {
             <div className="footer-con">
                 <div className="footer">
                     <div className="item-1">
-                        <i className="fab fa-linkedin-in footer-item-2"></i>
-                        <i className="fab fa-twitter footer-item-2"></i>
-                        <i className="fab fa-github-alt footer-item-2"></i>
-                        <i className="fab fa-free-code-camp footer-item-2"></i>
+                        <a href="https://www.linkedin.com/in/ezana-bahta/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in footer-item-2"></i></a>
+                        <a href="https://twitter.com/EzyBreezyCoolin" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter footer-item-2"></i></a>
+                        <a href="https://github.com/EzyBreezy" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-alt footer-item-2"></i></a>
+                        <a href="https://www.freecodecamp.org/ezybreezy" target="_blank" rel="noopener noreferrer"><i className="fab fa-free-code-camp footer-item-2"></i></a>
                     </div>
                     <div className="item-2">
                         {/* bottom of footer */}

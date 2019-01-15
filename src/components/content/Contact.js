2f3a199704cc6c4c42c333e23a72471a109b6b77
Ezana Bahta <ezanab2@gmail.com>
2019-01-14 23:18:14 -0500

    updated README.md looked at having projects organized by content type

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -7,12 +7,37 @@ class Contact extends Component {
             <div className="contact-con">
                 <h1 className="contact-title"> Contact Me </h1>
 
-                <form action="mailto:ezanab2@gmail.com" className="contact-form">
-                    <input type="text" placeholder="Name" className="input input-name"/>
-                    <input type="email" placeholder="Email*" required autocomplete="no" className="input input-phone"/>
-                    <input type="number" placeholder="Phone" className="input phone" />
-                    <textarea placeholder="Dont hesitate to shoot me a message!" className="input input-text" />
-                    <input type="submit" value="Submit" className="input input-submit" />
+                <form action="mailto:ezanab2@gmail.com" method="post" enctype="text/plain" className="contact-form">
+                    <input 
+                        type="text" 
+                        placeholder="Name" 
+                        name="Name" 
+                        className="input input-name"
+                        />
+                    <input 
+                        type="email" 
+                        placeholder="Email*" 
+                        name="my email is" 
+                        required 
+                        autocomplete="yes" 
+                        className="input input-phone"
+                        />
+                    <input 
+                        type="tel" 
+                        placeholder="Phone"
+                        name="my number is" 
+                        autocomplete="yes" 
+                        className="input phone" 
+                        />
+                    <textarea 
+                        placeholder="Dont hesitate to shoot me a message!" 
+                        className="input input-text" 
+                        />
+                    <input 
+                        type="submit" 
+                        value="Submit" 
+                        className="input input-submit" 
+                        />
                 </form>
             </div>
         );
--- src/components/content/Projects.js
+++ src/components/content/Projects.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
+import './Projects.css'
 
 class Projects extends Component {
+    constructor(props){
+        super(props);
+        this.state = { activeTab: 0}
+    }
+
     render() {
         return (
-            <div>
-                <h1> Projects Page </h1>
+            <div className="category-tabs-con">
+                <h1> Projects </h1>
+                <ul>
+                    <li>React</li>
+                    <li>Express</li>
+                    <li>MongoDB</li>
+                    <li>JavaScript</li>
+                    <li>HTML5</li>
+                </ul>
+
+                <section className="projects">
+                </section>
             </div>
         );
     }
